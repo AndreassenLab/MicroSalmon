86671ce5f164901983c0b37cf809fe5931ef76e3
LionOfComarre <teppicassassin@hotmail.com>
2021-09-13 22:26:07 +0200

    Changing -q list syntax

--- GO_ID_Search.py
+++ GO_ID_Search.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 
 
 parser = ArgumentParser()
-parser.add_argument("-q", "--query", help="Query IDs, one or more GO IDs separated by semicolons, e.g -q GO:0000050;GO:0004053")
+parser.add_argument("-q", "--query", help="Query IDs, one GO ID, or a list of multiple GO IDs in quotes separated by spaces, e.g -q \"GO:0000050 GO:0004053\"")
 parser.add_argument("-i", "--query_file", help="Query filename. Must be a list of GO IDs, one per line. At least one of Input_File or Query is required.")
 parser.add_argument("-p", "--prefix", help="Optional output prefix. If not included, output filename will be based only on the query ID.")
 parser.add_argument("-c", "--complexity", default=0.27, type=float, help="Minimum Trifonov Linguistic Complexity for Teiresias motifs. (Default: 0.27)")
@@ -158,7 +158,7 @@ for line in lines:
 queries = []
 
 if args.query:
-    queries += args.query.split(";")
+    queries += args.query.split(" ")
 
 if args.query_file:
     f=open(args.query_file,"r")
@@ -167,6 +167,9 @@ if args.query_file:
     for line in lines:
         queries.append(line.strip())
 
+while "" in queries:
+    queries.remove("")
+        
 for counter, query in enumerate(queries):
     print("Searching query " + str(counter+1) + " of " + str(len(queries)))
     skip = 0
